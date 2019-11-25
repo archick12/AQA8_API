fc15d56942e65ec5d97c5311b73051da4ad8bed1
archick12 <a.a.piluck@gmail.com>
2019-11-25 20:55:55 +0200

    added test for Create Issue

--- src/test/java/JIRAApiTest.java
+++ src/test/java/JIRAApiTest.java
@@ -2,28 +2,53 @@ import io.restassured.response.Response;
 import org.hamcrest.Matcher;
 import org.hamcrest.text.MatchesPattern;
 import org.testng.annotations.Test;
+
 import java.util.regex.Pattern;
+
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class JIRAApiTest {
 
-  @Test
-  public void
-  getIssue() {
+    @Test
+    public void getExistingIssue() {
 
-    Response response =
-        given().
-            auth().preemptive().basic("webinar5", "webinar5").
-            when().
-            get("http://jira.hillel.it/rest/api/2/issue/WEBINAR-8887").
-            then().
-            extract().response();
+        Response response =
 
-    assertEquals(200, response.statusCode());
-    assertEquals("WEBINAR-8887", response.path("key"));
-    final Matcher<String> matcher = new MatchesPattern(Pattern.compile("[A-Z]+\\-[0-9]+"));
-    assertTrue(matcher.matches("WEBINAR-8887"));
-  }
+                given().
+                        auth().preemptive().basic("webinar5", "webinar5").
+                        when().
+                        get("http://jira.hillel.it/rest/api/2/issue/WEBINAR-8887").
+                        then().
+                        extract().response();
+
+        assertEquals(200, response.statusCode());
+        assertEquals("WEBINAR-8887", response.path("key"));
+        final Matcher<String> matcher = new MatchesPattern(Pattern.compile("[A-Z]+\\-[0-9]+"));
+        assertTrue(matcher.matches("WEBINAR-8887"));
+    }
+
+
+    @Test
+    public void createIssue() {
+
+        String issueJSON = "{\"fields\":{" +
+                "\"summary\":\"Test summary\"," +
+                "\"issuetype\":{\"id\":\"10107\"}," +
+                "\"project\":{\"id\":\"11400\"}," +
+                "\"assignee\":{\"name\":\"webinar5\"}" +
+                "}}";
+
+        Response response = given().
+                auth().preemptive().basic("webinar5", "webinar5").
+                header("Content-Type","application/json").
+                body(issueJSON).
+                when().
+                post("https://jira.hillel.it/rest/api/2/issue").
+                then().
+                extract().response();
+
+        assertEquals(201, response.statusCode());
+    }
 }
